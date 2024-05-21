505644c6bbee0a1f288b8f56542af42c87ba1124
Travis-W-Moore <96447819+Travis-W-Moore@users.noreply.github.com>
2024-05-21 15:45:57 -0700

    Update create_features.sh

--- util/create_features.sh
+++ util/create_features.sh
@@ -3,12 +3,13 @@ size=$1
 name=$2
 genome=$3
 sizes=$4
+map=$5
 #Create window file
 Rscript window_file.R $size $name $sizes
 #Get gc content
 ./gc_content.sh $name $genome window_$name.bed
 #Get mappability
-./bigWigAverageOverBed wgEncodeCrgMapabilityAlign50mer.bigWig window_$name.bed mappability_50mer_$name.tab
+./bigWigAverageOverBed $map window_$name.bed mappability_50mer_$name.tab
 #create feature file
 Rscript combine_gc_map.R mappability_50mer_$name.tab GCcontent.$name.nuc.bed features.$name.csv
 #remove intermediate file
